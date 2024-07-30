26fd0f7e9f6150ae9401a00aea9c95d3e34aa961
wchu0036 <wchu0036@student.monash.edu>
2024-07-31 00:18:03 +1000

    complete task 2.1, 2.2

--- src/components/JSON.vue
+++ src/components/JSON.vue
@@ -10,17 +10,31 @@
       <h3>Iterating through Arrays</h3>
       <!-- Activity 6: Render a list containing author names and their birth years. Hint: Make use of the v-for directive to iterate through the array of authors. -->
       <!-- TODO: CODE TO RENDER LIST OF AUTHORS HERE -->
+      <ul>
+        <li v-for="author in authors" :key="author.id">
+          {{ author.name }} ({{ author.birthYear }})
+        </li>
+      </ul>
 
       <h3>Filtering Arrays</h3>
       <!-- Activity 7: Render a list containing authors born after 1850. Hint: Make use of the v-for directive to iterate through the array of authors that you have filtered out. -->
       <p>Authors born after 1850:</p>
       <!-- TODO: CODE TO RENDER LIST OF AUTHORS HERE -->
-
+      <ul>
+        <li v-for="author in modernAuthors" :key="author.id">
+          {{ author.name }} ({{ author.birthYear }})
+        </li>
+      </ul>
       <h3>Mapping Arrays</h3>
       <p>Famous works:</p>
       <ul>
         <!-- Activity 8: Render a list of all famous works. Hint: Use the v-for directive to iterate through the array of authors that you have filtered out. -->
         <!-- TODO: CODE TO RENDER LIST OF FAMOUS WORKS HERE -->
+        <ul>
+          <li v-for="work in allFamousWorks" :key="work">
+            {{ work }}
+          </li>
+        </ul>
       </ul>
 
       <h3>Finding in Arrays</h3>
@@ -30,6 +44,11 @@
       <p>{{ austen?.name }}'s works:</p>
       <!-- Activity 9: Render a list of Austen's works. Hint: Use the v-for directive to iterate through the array of authors that you have filtered out. -->
       <!-- TODO: CODE TO RENDER LIST OF AUSTEN'S WORKS HERE -->
+      <ul>
+        <li v-for="work in austen.famousWorks" :key="work">
+          {{ work.title }}
+        </li>
+      </ul>
     </section>
 
     <section class="lab-section">
@@ -41,29 +60,45 @@
         Company:
         <!-- Activity 9a: Get the company name from the bookstores object. -->
         <!-- TODO: CODE TO GET COMPANY NAME HERE -->
+        {{ bookstores.name }}
       </p>
 
       <p>
         Total Stores:
         <!-- Activity 9b: Get the total number of stores from the bookstores object. -->
         <!-- TODO: CODE TO GET TOTAL STORES HERE -->
+        {{ bookstores.totalStores }}
       </p>
 
       <h3>Iterating Object Properties</h3>
       <p>Store Types:</p>
       <!-- Activity 10: Iterate through the storeTypes array and display the store type and the number of stores that use that type. -->
       <!-- TODO: CODE TO RENDER LIST OF STORE TYPES HERE -->
+      <ul>
+        <li v-for="(value, key) in bookstores.storeTypes" :key="key">{{ key }}: {{ value }}</li>
+      </ul>
 
       <h3>Nested Objects</h3>
       <p>Opening Hours:</p>
       <!-- Activity 11: Iterate through the openingHours object and display the day of the week and the opening and closing times. -->
       <!-- TODO: CODE TO RENDER LIST OF OPENING HOURS HERE -->
+      <ul>
+        <li v-for="(value, key) in bookstores.openingHours" :key="key">
+          {{ key }}: {{ value.open }} - {{ value.close }}
+        </li>
+      </ul>
 
       <h3>Working with Arrays in Objects</h3>
       <!-- Activity 12: Get the top sellers from the bookstores object. -->
       <!-- TODO: CODE TO GET TOP SELLERS HERE -->
-      <p>We operate in:</p>
-      <p>Our #1 seller:</p>
+      <p>
+        We operate in:
+        <span v-for="country in bookstores.countries" :key="country">
+          {{ country }}
+          <span v-if="country != bookstores.countries[bookstores.countries.length - 1]">, </span>
+        </span>
+      </p>
+      <p>Our #1 seller: {{ bookstores.topSellers[0] }}</p>
     </section>
 
     <section class="lab-section">
@@ -72,13 +107,20 @@
       <!-- Activity 13: Toggle the message visibility when the button is clicked. -->
       <!-- TODO: CODE TO TOGGLE MESSAGE VISIBILITY HERE. Hint: Use the v-if directive. -->
       <button @click="showMessage = !showMessage">Toggle Message</button>
-      <p class="message success">✨ You're a Vue superstar! ✨</p>
-      <p>Click the button to see a message.</p>
+      <p v-if="showMessage" class="message success">✨ You're a Vue superstar! ✨</p>
+      <p v-else class="message">Click the button to see a message.</p>
     </section>
 
     <section class="lab-section">
       <h2>Attribute, Class and Style Binding with <code>v-bind</code></h2>
       <p>Highlighting Specific Authors:</p>
+      <ul>
+        <li v-for="author in authors" :key="author">
+          <span :class="{ highlight: author.name == orwell.name }">
+            {{ author.name }}
+          </span>
+        </li>
+      </ul>
     </section>
   </div>
 </template>
@@ -88,28 +130,35 @@ import { ref, computed } from 'vue'
 
 // Activity 1: Import JSON files (authors.json and bookstores.json)
 // TODO: CODE TO IMPORT JSON FILES HERE
-
+import authors from '../assets/json/authors.json'
+import bookstores from '../assets/json/bookstores.json'
 const showMessage = ref(false)
 
 // Activity 2: Get authors born after 1850
-const modernAuthors = computed(() => {
+const modernAuthors = computed(() =>
   // TODO: CODE TO FILTER ARRAY OF AUTHORS HERE
-})
+  authors.filter((author) => author.birthYear > 1850)
+)
 
 // Activity 3: Get all famous works
-const allFamousWorks = computed(() => {
+const allFamousWorks = computed(() =>
   // TODO: CODE TO GET ALL FAMOUS WORKS HERE
-})
+  authors.flatMap((author) => author.famousWorks.map((work) => work.title))
+)
 
 // Activity 4: Find author by name
-const orwell = computed(() => {
-  // TODO: CODE TO FIND AUTHOR BY NAME HERE
-})
+const orwell = computed(
+  () =>
+    // TODO: CODE TO FIND AUTHOR BY NAME HERE
+    authors.filter((author) => author.name == 'George Orwell')[0]
+)
 
 // Activity 5: Find author by ID
-const austen = computed(() => {
-  // TODO: CODE TO FIND AUTHOR BY ID HERE
-})
+const austen = computed(
+  () =>
+    // TODO: CODE TO FIND AUTHOR BY ID HERE
+    authors.filter((author) => author.id == 1)[0]
+)
 </script>
 
 <style scoped>
