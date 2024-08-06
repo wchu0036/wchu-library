87ef52d1fecfe3e50f365ac0d6389cd600798966
wchu0036 <wchu0036@student.monash.edu>
2024-08-06 11:10:00 +1000

    add commented code for task 3.1-3.4

--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -1,4 +1,33 @@
 <template>
+  <!--3.1-->
+  <!-- <div>
+    <h1>User Information Form / Credentials</h1>
+  </div> -->
+  <!-- 3.2 -->
+  <!-- <div class="form">
+    <h1>User Information Form / Credentials</h1>
+    <form>
+      <label for="username">Username:</label><br />
+      <input type="text" id="username" name="username" /><br />
+
+      <label for="password">Password:</label><br />
+      <input type="password" id="password" name="password" /><br />
+
+      <label for="isAustralian">Australian Resident?</label><br />
+      <input type="checkbox" id="isAustralian" name="isAustralian" /><br />
+
+      <label for="reason">Reason For Joining:</label><br />
+      <textarea id="reason" name="reason" rows="3"></textarea><br />
+
+      <label for="gender">Gender</label><br />
+      <select id="gender">
+        <option value="female">Female</option>
+        <option value="male">Male</option>
+        <option value="other">Other</option>
+      </select>
+    </form>
+  </div> -->
+  <!--3.5-->
   <div class="container mt-5">
     <div class="row">
       <div class="col-md-8 offset-md-2">
@@ -78,27 +107,6 @@
       </div>
     </div>
   </div>
-  <!-- <h1>User Information Form / Credentials</h1>
-  <form>
-    <label for="username">Username:</label><br />
-    <input type="text" id="username" name="username" /><br />
-
-    <label for="password">Password:</label><br />
-    <input type="password" id="password" name="password" /><br />
-
-    <label for="isAustralian">Australian Resident?</label><br />
-    <input type="checkbox" id="isAustralian" name="isAustralian" /><br />
-
-    <label for="reason">Reason For Joining:</label><br />
-    <textarea id="reason" name="reason" rows="3"></textarea><br />
-
-    <label for="gender">Gender</label><br />
-    <select id="gender">
-      <option value="female">Female</option>
-      <option value="male">Male</option>
-      <option value="other">Other</option>
-    </select>
-  </form> -->
 </template>
 
 <script setup>
