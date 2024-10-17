8808df585ea70f576c57bdc453cf8935a373cb49
wchu0036 <161562299+wchu0036@users.noreply.github.com>
2024-10-17 12:33:31 +0000

    Deploying to gh-pages from @ wchu0036/wchu-library@c7716ea5cbbb7ba1295a7f07c7089a9fb311a7e8 🚀

--- src/views/AccessDeniedView.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import store from '@/store/store'
-</script>
-<template>
-  <div class="denied-section" v-if="!store.state.isAuthenticated">
-    <h1>Access Denied...</h1>
-    <p>You are not authorised to view this page.</p>
-  </div>
-</template>
